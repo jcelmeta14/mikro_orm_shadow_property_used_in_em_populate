a778f6335c66f90525beb80b2d821a6127e8265a
Jurgen Celmeta <jur@gmail.com>
2025-12-02 16:00:02 +0100

    chore: finish reproduction

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
-# MikroORM reproduction example
+# Steps to reproduce
 
-This repository serves as a base reproduction example, it contains basic setup with MikroORM 6 with SQLite driver and jest. This is what the main repository is using, and therefore allows for a simple integration to the codebase.
+1. `docker compose up -d`
+2. `npm test`
 
-## Few hints for creating your own reproductions
+## Notes
 
-- Focus on reproducing one problem at a time.
-- Set up the data, the test needs to be **self-contained**.
-- Don't use the CLI, do everything programmatically as part of the test.
-- Remove everything unrelated, keep things as simple as possible.
-- Duplication in tests is fine, better than complex abstractions.
-- Comments are fine, asserts are better!
-- If the problem is not driver specific, use in-memory SQLite database.
+1. The problem seems to happend when having 3 entities that are related 1:M 1:M
+2. The leaf entity needs to have an embeddable with a shadow property
+3. The shadow property is used in query instead of correctly being ignored because it should not be persisted
--- src/example.test.ts
+++ src/example.test.ts
@@ -1,4 +1,4 @@
-import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/sqlite';
+import { Collection, Embeddable, Embedded, Entity, ManyToOne, MikroORM, OneToMany, PostgreSqlDriver, PrimaryKey, Property, Ref } from '@mikro-orm/postgresql';
 
 @Entity()
 class User {
@@ -12,6 +12,9 @@ class User {
   @Property({ unique: true })
   email: string;
 
+  @OneToMany(() => Book, (book) => book.author)
+  books = new Collection<Book>(this);
+
   constructor(name: string, email: string) {
     this.name = name;
     this.email = email;
@@ -19,14 +22,62 @@ class User {
 
 }
 
+@Entity()
+class Book {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  title!: string;
+
+  @ManyToOne(() => User)
+  author!: Ref<User>;
+
+  @OneToMany(() => Chapter, (chapter) => chapter.book)
+  chapters = new Collection<Chapter>(this);
+}
+
+@Embeddable()
+class NestedChapterProperties {
+  @Property()
+  someEmbeddedProperty!: string;
+
+  @Property({ persist: false, hidden: true })
+  private someEmbeddedShadowProperty!: string;
+
+  setSomeEmbeddedShadowProperty(value: string) {
+    this.someEmbeddedShadowProperty = value;
+  }
+}
+
+@Entity()
+class Chapter {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name!: string;
+
+  @Embedded(() => NestedChapterProperties)
+  nestedProperties!: NestedChapterProperties;
+
+  @ManyToOne(() => Book)
+  book!: Ref<Book>;
+}
+
 let orm: MikroORM;
 
 beforeAll(async () => {
   orm = await MikroORM.init({
-    dbName: ':memory:',
-    entities: [User],
+    driver: PostgreSqlDriver,
+    dbName: 'reproduction',
+    entities: [User, Book, Chapter],
     debug: ['query', 'query-params'],
     allowGlobalContext: true, // only for testing
+    user: 'reproduction',
+    password: 'reproduction',
+    host: 'localhost',
+    port: 54322,
   });
   await orm.schema.refreshDatabase();
 });
@@ -35,17 +86,31 @@ afterAll(async () => {
   await orm.close(true);
 });
 
-test('basic CRUD example', async () => {
-  orm.em.create(User, { name: 'Foo', email: 'foo' });
-  await orm.em.flush();
-  orm.em.clear();
-
-  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
-  expect(user.name).toBe('Foo');
-  user.name = 'Bar';
-  orm.em.remove(user);
+test('populating user books after modifying a user should not include shadow property in query issued to the database', async () => {
+  // Setup test
+  const user = orm.em.create(User, { name: 'Foo', email: 'foo' });
+  const book = orm.em.create(Book, { title: 'Book 1', author: user });
+  const nestedChapterProperties = orm.em.create(NestedChapterProperties, { someEmbeddedProperty: 'Some Embedded Property 1' });
+  nestedChapterProperties.setSomeEmbeddedShadowProperty('Do some specifc business logic here');
+  const chapter = orm.em.create(Chapter, { name: 'Some Other Entity 1', book, nestedProperties: nestedChapterProperties });
   await orm.em.flush();
 
-  const count = await orm.em.count(User, { email: 'foo' });
-  expect(count).toBe(0);
+  // Create forked entity manager to get a fresh 
+  const forkedEm = orm.em.fork({ clear: true });
+
+  const updatedUserWithBooks = await forkedEm.transactional(async (em) => {
+    const user = await em.findOneOrFail(User, { email: 'foo' });
+
+    // Emulate some business logic that modifies the user
+    user.name = 'Bar';
+
+    await em.flush();
+
+    // Load books, the problem is here the query that is generated is selecting the a column based on the shadow property name
+    await em.populate(user, ['books', 'books.chapters']);
+
+    return user;
+  })
+
+  expect(updatedUserWithBooks.books.length).toBe(1);
 });
